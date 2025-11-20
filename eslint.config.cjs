1a8a60d90c96a63375edf7b9493f633dff5c035e
Camila Arruda <deco260483@gmail.com>
2025-11-20 09:28:38 -0300

    fix: add eslint.config.cjs for ESLint v9 compatibility

---
+++ eslint.config.cjs
@@ -0,0 +1,7 @@
+// eslint.config.cjs
+module.exports = {
+  env: { node: true, es2021: true, jest: true },
+  extends: ["eslint:recommended"],
+  parserOptions: { ecmaVersion: 12 },
+  rules: {}
+};
