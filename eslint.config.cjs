adb2bd85b3721632b0ef1d90d010c31b6d0e0366
Camila Arruda <deco260483@gmail.com>
2025-11-20 09:36:43 -0300

    chore: migrate to ESLint flat config (languageOptions + globals)

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,7 +1,29 @@
 // eslint.config.cjs
-module.exports = {
-  env: { node: true, es2021: true, jest: true },
-  extends: ["eslint:recommended"],
-  parserOptions: { ecmaVersion: 12 },
-  rules: {}
-};
+const globals = require("globals");
+
+module.exports = [
+  {
+    // aplica em todos os .js (ajuste se quiser)
+    files: ["**/*.js"],
+
+    // aqui substituímos "env" pelo languageOptions
+    languageOptions: {
+      // traz as globals padrão do node + jest (readonly por padrão)
+      globals: {
+        ...globals.node,
+        ...globals.jest
+      },
+
+      // parserOptions agora dentro de languageOptions
+      parserOptions: {
+        ecmaVersion: 12,
+        sourceType: "script"
+      }
+    },
+
+    // regras base (mesma ideia do "extends": "eslint:recommended")
+    // para usar presets você pode importar/usar preset functions, mas
+    // a forma simples para regras é conforme abaixo:
+    rules: {}
+  }
+];
