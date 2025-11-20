f2410e6a5dbed9db5b3ef6658ae74d8d3348a90c
Camila Arruda <deco260483@gmail.com>
2025-11-20 10:53:33 -0300

    chore: eslint flat config + remove node_modules from repo

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -3,27 +3,22 @@ const globals = require("globals");
 
 module.exports = [
   {
-    // aplica em todos os .js (ajuste se quiser)
     files: ["**/*.js"],
 
-    // aqui substituímos "env" pelo languageOptions
     languageOptions: {
-      // traz as globals padrão do node + jest (readonly por padrão)
+      // define as globals (em vez de usar "env")
       globals: {
         ...globals.node,
         ...globals.jest
       },
 
-      // parserOptions agora dentro de languageOptions
       parserOptions: {
         ecmaVersion: 12,
         sourceType: "script"
       }
     },
 
-    // regras base (mesma ideia do "extends": "eslint:recommended")
-    // para usar presets você pode importar/usar preset functions, mas
-    // a forma simples para regras é conforme abaixo:
+    // regras base; adicione regras aqui se quiser
     rules: {}
   }
 ];
